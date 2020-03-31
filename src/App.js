7f30860c0b316173c19a1840086a64babe5f6151
RunEagler <ohk.cotf1649@gmail.com>
2020-04-01 08:37:38 +0900

    add: useState lecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React,{useState} from 'react';
+
 
 function App() {
+  const [count,setCount] = useState(0)
+  const increment = ()=> setCount(count+1)
+  const decrement = ()=> setCount(count-1)
+  const increment2 = () => setCount(prevCount=>prevCount+1)
+  const reset = ()=>setCount(0)
+  const double =  ()=>setCount(count*2)
+  const divThree = ()=>setCount(c=> c%3==0? c/3 : c)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>{count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <div>
+        <button onClick={increment2}>+2</button>
+      </div>
+      <div>
+        <button onClick={reset}>reset</button>
+        <button onClick={double}>double</button>
+        <button onClick={divThree}>div3</button>
+      </div>
+    </>
   );
 }
 
