273f880dd2c96ddbf02f814b1b548b5152d2bf25
RunEagler <ohk.cotf1649@gmail.com>
2020-04-02 21:22:23 +0900

    add: actions

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { DELETE_EVENT } from '../actions';
 
 const Event = ({ event, dispatch }) => {
 
   const handleClickDeleteEvent = () => {
     const result = window.confirm(`イベント「${event.title}」を消してしまってもよろしいでしょうか？`)
-    if (result) {dispatch({ type: 'DELETE_EVENT', id:event.id })};
+    if (result) {dispatch({ type:DELETE_EVENT, id:event.id })};
   };
   return (
     <tr>
